cae458ac130b111b66d68cfef9b71126c450d31e
traysonkelii <traysonkelii@gmail.com>
2021-01-20 20:42:53 -0700

    got frontend working

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
 var path = require('path');
 
-router.get('/', function (req, res, next) {
-	var filePath = path.join(__dirname, '..', 'dist/angular-consync', 'index.html');
-	res.sendFile(filePath);
-	res.json('This will be the homepage')
-});
-
-// router.use('/', require('./auth/auth'))
+const root = path.join(__dirname, '..', '..', '..', 'app/public');
+router.use('/', require('./auth/auth'))
 router.use('/healthcheck', require('./healthcheck'));
 router.use('/project', require('./project/projectIndex'));
 router.use('/user', require('./user/userIndex'));
+
+router.get('*.*', express.static(root, { maxAge: '1y' }));
+router.all('*', function (req, res) {
+	res.status(200).sendFile(`/`, { root: root });
+});
+
+router.get('*', function (req, res, next) {
+	res.sendFile(root, { maxAge: '1y' });
+});
 
 module.exports = router;
