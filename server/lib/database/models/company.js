b0bcc252522a60132138a98bcfd3ddc23fff8667
Joshua Christensen <jcawesome@gmail.com>
2021-02-13 11:44:38 -0700

    adding companies and users

--- server/lib/database/models/company.js
+++ server/lib/database/models/company.js
@@ -2,15 +2,15 @@ let mongoose = require('mongoose');
 
 let CompanySchema = mongoose.Schema(
 	{
-		title: {type: String, required: 'title is a required field'},
+		name: {type: String, required: 'name is a required field'},
 		domain: {type: String, required: 'domain is a required field'},
-		description: {type: String},
+		description: {type: String}
 	}, {
 	timestamps: true,
-	versionKey: false, // removing mongoose versionkey because it prevents updating the document when you don't provIde a new version
+	versionKey: false, //removing mongoose versionkey because it prevents updating the document when you don't provIde a new version
 	minimize: false,
 	collection: 'companies'
-}
+	}
 );
 
 module.exports = mongoose.model('Company', CompanySchema);
---
+++ server/mockDatabase/companies.js
@@ -0,0 +1,14 @@
+db.companies.insertMany([
+	{
+		_id: ObjectId("606fa8e6bc8001095fd45af9"),
+		name: 'Drywallers Extreme',
+		description: 'We do drywall.',
+		domain: 'drywall.co'
+	},
+	{
+		_id: ObjectId("606fa8e6bc8001095fd45af0"),
+		name: 'Sal\'s Plumbing',
+		description: 'We do plumbing.',
+		domain: 'salsplumbing.com'
+	}
+]);
